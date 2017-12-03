c3d0de321eed279ef9709f47a9fc83df199985f0
Jerry Reed <greed9@valenciacollege.edu>
2017-12-02 19:38:34 -0500

    Cleaned up on/off code, removed dimmer stuff for now (won't work on DUE)

--- sketch_timer_remote2/sketch_timer_remote2.ino
+++ sketch_timer_remote2/sketch_timer_remote2.ino
@@ -1,138 +1,27 @@
 #include <DS3231.h>
-#include <TimerOne.h>           // Avaiable from http://www.arduino.cc/playground/Code/Timer1
 
 // Code from the Demo Example of the DS3231 Library
 DS3231  rtc(SDA, SCL);
 
-#define ON 0
-#define OFF 1
+byte ch3OnPin = 6 ;
+byte ch3OffPin = 7 ;
+byte ch1OnPin = 10 ;
+byte ch1OffPin = 9 ;
 
-byte onPin = 6 ;
-byte offPin = 7 ;
-
-// Turn on wireless remote
-void turnOn ( int nTimes )
+// Turn on/off wireless remotes
+void activateRemote ( byte pin )
 {
-  for ( int i = 0 ; i < nTimes ; i++ )
+  for ( int i = 0 ; i < 3 ; i++ )
   {
-    digitalWrite ( onPin, HIGH ) ;
+    digitalWrite ( pin, HIGH ) ;
     delay ( 1000 ) ;
-    digitalWrite ( onPin, LOW ) ;
+    digitalWrite ( pin, LOW ) ;
     delay ( 1000 ) ;
   }
-}
-
-// Turn off wireless remote
-void turnOff ( int nTimes )
-{
-  for ( int i = 0 ; i < nTimes ; i++ )
-  {
-    digitalWrite ( offPin, HIGH ) ;
-    delay ( 1000 ) ;
-    digitalWrite ( offPin, LOW ) ;
-    delay ( 1000 ) ;
-  }
-}
-
-// zero-crossing sync dimming using:
-// https://www.amazon.com/RobotDyn-controller-control-Arduino-Raspberry/dp/B072K9P7KH/ref=sr_1_1?ie=UTF8&qid=1512008507&sr=8-1&keywords=robotdyn+dimmer
-
-class Dimmer 
-{
-  public:
-    int acPin = 3 ;             // output to dimmer pwm pin
-    int dim = 128 ;             // dimming level 128 is off, 0 is full on
-    int freqStep = 65 ;         // magic number for 60 hz timing
-    volatile int intCount = 0 ; // Number of zero crossings so far
-    volatile int ts = 0 ;       // time step
-    volatile boolean zeroCross = 0 ;
-    int stepCount = 8 ;         // change dim by this amount each time?
-
-    Dimmer ( ) 
-    {
-      initialize ( ) ;
-    }
-
-    Dimmer ( int pPin, int pStartLevel = 128 )
-    {
-      acPin = pPin ;
-      dim = pStartLevel ;
-      initialize ( ) ;
-    }
-
-    void initialize ( )
-    {
-      pinMode(acPin, OUTPUT);                          // Set the Triac pin as output
-      attachInterrupt(0, zeroCrossWrapper, RISING);    // Attach an Interupt to Pin 2 (interupt 0) for Zero Cross Detection
-      Timer1.initialize(freqStep);                      // Initialize TimerOne library for the freq we need
-      Timer1.attachInterrupt(dimCheckWrapper, freqStep);      // Go to dim_check procedure every 75 uS (50Hz)  or 65 uS (60Hz)
-  
-    }
-
-    // Actual ISR's w/in object, wrapped with external global func
-    void zeroCrossISR ( )
-    {
-      zeroCross = true;               // set flag for dim_check function that a zero cross has occured
-      ts = 0;                             // stepcounter to 0.... as we start a new cycle
-      digitalWrite(acPin, LOW);
-      intCount ++ ;
-    }
-
-    // Actual ISR's w/in object, wrapped with external global func
-    void dimCheckISR ( )
-    {
-        if(zeroCross == true)
-        {              
-          if(ts>=dim) 
-          {                     
-            digitalWrite(acPin, HIGH);  // turn on light       
-            ts = 0 ;  // reset time step counter                         
-            zeroCross = false;    // reset zero cross detection flag
-          } 
-          else 
-          {
-            ts++;  // increment time step counter                     
-          }    
-      }
-    }
-
-    // Brighten
-    void increase ( int pStep )
-    {
-      dim += pStep ;
-      if ( dim > 127 )
-      {
-        dim = 127 ;
-      }
-    }
-
-    // Dim the power
-    void decrease ( int pStep )
-    {
-      dim -= pStep ;
-      if ( dim < 0 )
-      {
-        dim = 0 ;
-      }
-    }
-} ;
-
-Dimmer dimmer ( 3 ) ;
-
-// Global wrapper function for ISR.  Deals with "this" argument problem
-void zeroCrossWrapper ( ) 
-{
-  dimmer.zeroCrossISR ( ) ;
-}
-
-// Global wrapper function for ISR.  Deals with "this" argument problem
-void dimCheckWrapper ( )
-{
-  dimmer.dimCheckISR ( ) ;
 }
 
 // Pointer to ISR function
-typedef void (*PinFunc ) ( int);
+typedef void (*PinFunc ) ( byte);
 
 // Timed event.  When time is met, call the eventFunction
 class TimerEvent
@@ -188,10 +77,12 @@ class TimerEvent
     }
 } ; // end TimerEvent class
 
-TimerEvent lightON ( "17", "00", "Lights ON!", turnOn, 3 ) ;
-TimerEvent lightOFF ( "23", "59", "Lights OFF!", turnOff, 3 ) ;
-//TimerEvent lightON ( "10", "21", ON, "Lights ON!" ) ;
-//TimerEvent lightOFF ( "10", "23", OFF, "Lights OFF!" ) ;
+// Porch lights on at 5 PM, off at midnight
+// xmas lights on at 5:15PM, off at 3 AM.
+TimerEvent porchLightON ( "17", "00", "Porch lights ON!", activateRemote, ch3OnPin ) ;
+TimerEvent porchLightOFF ( "23", "59", "Portch lights OFF!", activateRemote, ch3OffPin ) ;
+TimerEvent xmasLightsON ( "17", "15", "Xmas Lights On!", activateRemote, ch1OnPin ) ;
+TimerEvent xmasLightsOFF ( "03", "00", "Xmas Lights Off!", activateRemote, ch1OffPin ) ;
 
 void setup()
 {
@@ -202,16 +93,20 @@ void setup()
   rtc.begin();
 
   // Output pins
-  pinMode ( onPin, OUTPUT ) ;
-  pinMode ( offPin, OUTPUT ) ;
-
+  pinMode ( ch3OnPin, OUTPUT ) ;
+  pinMode ( ch3OffPin, OUTPUT ) ;
+  pinMode ( ch1OnPin, OUTPUT ) ;
+  pinMode ( ch1OffPin, OUTPUT ) ;
+  
   // The following lines can be uncommented to set the date and time
   //rtc.setDOW(FRIDAY);     // Set Day-of-Week to SUNDAY
   //rtc.setTime(22, 36, 0);     // Set the time to 12:00:00 (24hr format)
   //rtc.setDate(( uint8_t ) 24, ( uint8_t ) 11, ( uint16_t ) 2017);   // Set the date to January 1st, 2014
   delay ( 500 ) ;
 
-  turnOff ( 3 ) ;
+  // start off
+  activateRemote ( ch3OffPin ) ;
+  activateRemote ( ch1OffPin ) ;
 }
 
 void loop()
@@ -228,8 +123,11 @@ void loop()
   Serial.println(rtc.getTimeStr());
 
   // Wait one second before repeating
-  delay (1000);
+  delay (10000);
 
-  lightON.tick ( rtc.getTimeStr ( ) ) ;
-  lightOFF.tick ( rtc.getTimeStr ( ) ) ;
+  porchLightON.tick ( rtc.getTimeStr ( ) ) ;
+  porchLightOFF.tick ( rtc.getTimeStr ( ) ) ;
+  xmasLightsON.tick ( rtc.getTimeStr ( ) ) ;
+  xmasLightsOFF.tick ( rtc.getTimeStr ( ) ) ;
+  
 }
